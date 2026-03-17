04e1fadaaa390ddb29c1de612cf4f474692dc643
Luisa <nejiparra12@gmail.com>
2026-03-17 06:21:40 -0500

    Revert "Merge branch 'main' into feature"
    This reverts commit 0cab136687c0e9e4b7a4644bcfb96a1c7900ed91, reversing
    changes made to e8797f12e139c1c020bd58b2449a79f7cf502a00.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,4 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
 }
